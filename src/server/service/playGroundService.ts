52c1f99a21b4e572f05476dacde5307c880fa68f
854 <=>
2023-01-21 22:46:35 +0900

    add playgroundservice

---
+++ src/server/service/playGroundService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { Express } from "express";
+
+export default (app: Express) => {
+  console.log("playGround Service start");
+
+  app.get("/playground", async (req, res) => {
+    const reqest = await axios.post("http://localhost:3018/user", {
+      username: "hello",
+      salt: "hello",
+      hash: "hello",
+      profile: "hello",
+    });
+    // console.log(reqest);
+
+    res.json({ status: "ok" });
+  });
+
+  console.log("playGround Service end");
+};
