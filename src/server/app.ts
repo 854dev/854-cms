f23fb0ffe4288166e1b66cb8f4a196c2c7c3e5ea
854 <=>
2023-01-21 22:51:50 +0900

    edti app 미들웨어 추가

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 export default app;
