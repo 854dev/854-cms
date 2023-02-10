45a7bd2acd2c8d53c817d1b00dd41f6ee038b6f9
854 <=>
2023-02-10 16:07:29 +0900

    add data-source

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,23 @@ import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { ContentModule } from './content/content.module';
 import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/db-config-service';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
     AccountModule,
     ContentModule,
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
