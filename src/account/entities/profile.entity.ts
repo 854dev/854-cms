b321748e068fa67b3153442c83a0b46fca760831
854 <=>
2023-02-13 03:27:05 +0900

    edit entity

--- src/account/entities/profile.entity.ts
+++ src/account/entities/profile.entity.ts
@@ -1,9 +1,16 @@
-import { BaseEntity, Column, Entity, OneToOne } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 import { User } from './user.entity';
 
 @Entity()
 export class Profile extends BaseEntity {
+  @PrimaryGeneratedColumn()
   @OneToOne(() => User, (user) => user.user_id)
   user_id: number;
 
