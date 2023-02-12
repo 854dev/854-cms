12e19ea75a9e5520e83fede0c9d59d5fab361a60
854 <=>
2023-02-13 03:23:09 +0900

    add scripts

--- src/scripts/migration-create.ts
+++ src/scripts/migration-create.ts
@@ -3,7 +3,7 @@ import * as shell from 'shelljs';
 function main() {
   dotenv.config();
   shell.exec(
-    'yarn typeorm migration:generate ./src/database/migrations/migration -d ./src/database/data-source.ts'
+    'yarn typeorm migration:create ./src/database/migrations/migration'
   );
 }
 main();
