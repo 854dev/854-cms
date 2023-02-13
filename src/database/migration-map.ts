56ecc0a0c74d1a6a9becb87b31a316356e6c7954
854 <jahae854@gmail.com>
2023-02-14 03:26:04 +0900

    migration 230214

--- src/database/migration-map.ts
+++ src/database/migration-map.ts
@@ -1,6 +1,11 @@
 import { migration1676267856094 } from './migrations/1676267856094-migration';
 import { migration1676300794453 } from './migrations/1676300794453-migration';
+import { migration1676303831869 } from './migrations/1676303831869-migration';
 
-const migrations = [migration1676267856094, migration1676300794453];
+const migrations = [
+  migration1676267856094,
+  migration1676300794453,
+  migration1676303831869,
+];
 
 export default migrations;
