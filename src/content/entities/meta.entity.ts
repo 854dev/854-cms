e6edcd15f7a1eb94ac2b281aa9a5aaaf26673c16
854 <jahae854@gmail.com>
2023-02-15 20:53:48 +0900

    add content entity

--- src/content/entities/meta.entity.ts
+++ src/content/entities/meta.entity.ts
@@ -1,1 +1,42 @@
-export class Meta {}
+import { Type } from 'class-transformer';
+import { User } from 'src/account/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class Meta {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'int' })
+  @JoinColumn()
+  @OneToOne(() => User, (user) => user.id, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  contentId: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  title: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  creator: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @Column({ type: 'varchar', length: 100 })
+  status: string;
+}
