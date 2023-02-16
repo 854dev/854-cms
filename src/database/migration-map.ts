04ae90ada53ca6f1f16dc2b88c0e34a972b8e14d
854 <=>
2023-02-16 13:16:36 +0900

    migration 1676520941408

--- src/database/migration-map.ts
+++ src/database/migration-map.ts
@@ -2,12 +2,14 @@ import { migration1676267856094 } from './migrations/1676267856094-migration';
 import { migration1676300794453 } from './migrations/1676300794453-migration';
 import { migration1676303831869 } from './migrations/1676303831869-migration';
 import { migration1676520705673 } from './migrations/1676520705673-migration';
+import { migration1676520941408 } from './migrations/1676520941408-migration';
 
 const migrations = [
   migration1676267856094,
   migration1676300794453,
   migration1676303831869,
   migration1676520705673,
+  migration1676520941408,
 ];
 
 export default migrations;
