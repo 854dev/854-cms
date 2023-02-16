f824c21f2771832227953712edb694cbde2aa458
854 <=>
2023-02-16 13:15:34 +0900

    edit entity ContentBody

--- src/content/entities/content-body.entity.ts
+++ src/content/entities/content-body.entity.ts
@@ -1,9 +1,12 @@
-import { Column, Entity, JoinColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('content_body')
 export class ContentBody {
-  @JoinColumn()
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @JoinColumn()
+  contentId: number;
 
   @Column({ type: 'varchar', length: 100 })
   type_name: string;
