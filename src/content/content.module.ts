fc0eaac1f04148b93d2ca5974f6435bc838236ab
854 <=>
2023-02-16 13:10:10 +0900

    edit content module

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ContentController } from './content.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entityMap from 'src/database/entity-map';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([...entityMap.content])],
   controllers: [ContentController],
-  providers: [ContentService]
+  providers: [ContentService],
 })
 export class ContentModule {}
