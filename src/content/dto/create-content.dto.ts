46812010c5fdeca45413d3dc259cce207d69ff88
854 <jahae854@gmail.com>
2023-02-17 00:56:52 +0900

    edit dto

--- src/content/dto/create-content.dto.ts
+++ src/content/dto/create-content.dto.ts
@@ -1,1 +1,27 @@
-export class CreateContentDto {}
+import { IsInt, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateContentDto {
+  // type
+  @IsInt()
+  contentTypeId: number;
+  // meta
+  @IsString()
+  @MaxLength(100)
+  @MinLength(1)
+  title: string;
+
+  @IsString()
+  @MaxLength(100)
+  @MinLength(1)
+  creator: string;
+
+  @IsString()
+  @MaxLength(100)
+  @MinLength(1)
+  // TODO 별도 validator 로 검증 필요
+  status: string;
+
+  // body
+  // TODO 별도 validator 로 검증
+  body: any;
+}
--- src/content/dto/update-content.dto.ts
+++ src/content/dto/update-content.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsInt } from 'class-validator';
 import { CreateContentDto } from './create-content.dto';
 
-export class UpdateContentDto extends PartialType(CreateContentDto) {}
+export class UpdateContentDto extends PartialType(CreateContentDto) {
+  @IsInt()
+  id: number;
+}
