4d8bed02139207039d01eb961c0ba74db05d487e
854 <jahae854@gmail.com>
2023-02-20 11:14:00 +0900

    edit ContentType entity

--- src/content-type/entities/content-type.entity.ts
+++ src/content-type/entities/content-type.entity.ts
@@ -1,10 +1,15 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ContentCore } from '../../content/entities/content-core.entity';
+import { ContentBodyField } from './content-body-field.entity';
 
 @Entity('content_type')
 export class ContentType {
   @PrimaryGeneratedColumn()
   @OneToMany(() => ContentCore, (contentCore) => contentCore.id, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @OneToMany(() => ContentBodyField, (cbf) => cbf.contentTypeId, {
     nullable: false,
     onDelete: 'CASCADE',
   })
