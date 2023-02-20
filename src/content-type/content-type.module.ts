df62cbc78d8e80be438efbf853319d0a6582ed1b
854 <jahae854@gmail.com>
2023-02-20 14:14:03 +0900

    edit ContentTypeModule : imports

--- src/content-type/content-type.module.ts
+++ src/content-type/content-type.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ContentTypeService } from './content-type.service';
 import { ContentTypeController } from './content-type.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContentBodyField } from './entities/content-body-field.entity';
+import { ContentType } from './entities/content-type.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ContentBodyField, ContentType])],
   controllers: [ContentTypeController],
-  providers: [ContentTypeService]
+  providers: [ContentTypeService],
 })
 export class ContentTypeModule {}
