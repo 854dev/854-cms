489ae870e97a63e66b14cb381cea7b4a65e95f2f
854 <=>
2023-02-21 14:35:33 +0900

    delete migration

--- src/database/migration-map.ts
+++ src/database/migration-map.ts
@@ -1,29 +1,3 @@
-import { migration1676267856094 } from './migrations/1676267856094-migration';
-import { migration1676300794453 } from './migrations/1676300794453-migration';
-import { migration1676303831869 } from './migrations/1676303831869-migration';
-import { migration1676520705673 } from './migrations/1676520705673-migration';
-import { migration1676520941408 } from './migrations/1676520941408-migration';
-import { migration1676522287342 } from './migrations/1676522287342-migration';
-import { migration1676523948419 } from './migrations/1676523948419-migration';
-import { migration1676530928218 } from './migrations/1676530928218-migration';
-import { migration1676532126169 } from './migrations/1676532126169-migration';
-import { migration1676533822687 } from './migrations/1676533822687-migration';
-import { migration1676537033106 } from './migrations/1676537033106-migration';
-import { migration1676537344921 } from './migrations/1676537344921-migration';
-
-const migrations = [
-  migration1676267856094,
-  migration1676300794453,
-  migration1676303831869,
-  migration1676520705673,
-  migration1676520941408,
-  migration1676522287342,
-  migration1676523948419,
-  migration1676530928218,
-  migration1676532126169,
-  migration1676533822687,
-  migration1676537033106,
-  migration1676537344921,
-];
+const migrations = [];
 
 export default migrations;
