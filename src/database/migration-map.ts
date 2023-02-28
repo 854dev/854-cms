5bbe23c26716d705513254b4835a34ef7dc60563
854 <jahae854@gmail.com>
2023-02-28 22:31:22 +0900

    edit migrations

--- src/database/migration-map.ts
+++ src/database/migration-map.ts
@@ -1,6 +1,3 @@
-import { migration1677483504837 } from './migrations/1677483504837-migration';
-import { migration1677519532894 } from './migrations/1677519532894-migration';
-
-const migrations = [migration1677483504837, migration1677519532894];
+const migrations = [];
 
 export default migrations;
