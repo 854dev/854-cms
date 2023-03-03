e574271211aade1cc289b2dbbb8668d9e8df7629
854 <=>
2023-03-03 15:23:46 +0900

    edit content type module

--- src/content-type/content-type.module.ts
+++ src/content-type/content-type.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ContentTypeService } from './content-type.service';
 import { ContentTypeController } from './content-type.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ContentType } from './entities/content-type.entity';
-import { ContentBodySchema } from 'src/content-type/entities/content-body-schema.entity';
 import { ContentBodySchemaController } from './content-body-schema.controller';
 import { ContentBodySchemaService } from './content-body-schema.service';
+import entityMap from 'src/database/entity-map';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ContentType, ContentBodySchema])],
+  imports: [TypeOrmModule.forFeature([...entityMap.contentType])],
   controllers: [ContentTypeController, ContentBodySchemaController],
   providers: [ContentTypeService, ContentBodySchemaService],
 })
