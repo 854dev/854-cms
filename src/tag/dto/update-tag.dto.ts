5c68d97dd82881ee8193221447c4200282eff133
854 <jahae854@gmail.com>
2023-03-13 18:49:19 +0900

    edit dto

--- src/tag/dto/update-tag.dto.ts
+++ src/tag/dto/update-tag.dto.ts
@@ -1,6 +1,9 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsInt } from 'class-validator';
 import { CreateTagDto } from './create-tag.dto';
 
 export class UpdateTagDto extends PartialType(CreateTagDto) {
+  @ApiProperty()
+  @IsInt()
   tagId: number;
 }
